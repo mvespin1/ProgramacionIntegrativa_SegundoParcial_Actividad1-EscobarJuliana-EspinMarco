bae1067018c468cecfdf8a2960ef0fd3be96034a
mvespin1 <107557153+mvespin1@users.noreply.github.com>
2024-01-05 13:10:13 -0500

    actividad1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 class="title">{{ msg }}</h1>
+    <input v-model="msg" class="input" /> <br><br>
+    <button @click="resetMessage" class="button">Restablecer</button> <br>
+    <button @click="uppercaseMessage" class="button">Mayúsculas</button> <br>
+    <button @click="lowercaseMessage" class="button">Minúsculas</button><br>
+    <button @click="reverseMessage" class="button">Reverso</button> <br>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const msg = ref('¡Hola, escribe un texto!')
+
+const resetMessage = () => {
+  msg.value = '¡Hola, escribe un texto!';
+};
+
+const uppercaseMessage = () => {
+  msg.value = msg.value.toUpperCase();
+};
+
+const lowercaseMessage = () => {
+  msg.value = msg.value.toLowerCase();
+};
+
+const reverseMessage = () => {
+  msg.value = msg.value.split('').reverse().join('');
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.title {
+  color: #900C3F;
+  font-size: 24px;
+  margin-bottom: 16px;
+}
+
+.input {
+  padding: 8px;
+  margin-bottom: 16px;
+}
+
+.button {
+  padding: 8px 16px;
+  margin-bottom: 8px;
+  background-color: #900C3F;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 4px;
+}
+
+.button:hover {
+  background-color: #702736;
 }
 </style>
